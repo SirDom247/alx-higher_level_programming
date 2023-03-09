19d08ddd4f55b19cacfbf073403571dfbec0c0fc
SirDom247 <dominicudousoro@gmail.com>
2023-03-09 14:53:54 -0800

    python-import_modules

---
+++ 0x02-python-import_modules/calculator_1.py
@@ -0,0 +1,43 @@
+#!/usr/bin/python3
+def add(a, b):
+    """Addition
+    Args:
+        a: first integer
+        b: second integer
+    Returns:
+        The return value. a + b
+    """
+    return (a + b)
+
+
+def sub(a, b):
+    """Subtraction
+    Args:
+        a: first integer
+        b: second integer
+    Returns:
+        The return value. a - b
+    """
+    return (a - b)
+
+
+def mul(a, b):
+    """Multiplication
+    Args:
+        a: first integer
+        b: second integer
+    Returns:
+        The return value. a * b
+    """
+    return (a * b)
+
+
+def div(a, b):
+    """Division
+    Args:
+        a: first integer
+        b: second integer
+    Returns:
+        The return value. a / b
+    """
+    return int(a / b)
