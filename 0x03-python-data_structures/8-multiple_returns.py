a909be68beeee4fe5dcff3b140ddc0d39953ef78
SirDom247 <dominicudousoro@gmail.com>
2023-03-13 18:58:47 -0700

    python data structures

--- 0x03-python-data_structures/8-multiple_returns.py
+++ 0x03-python-data_structures/8-multiple_returns.py
@@ -1,8 +1,8 @@
 #!/usr/bin/python3
+
 def multiple_returns(sentence):
-    s_tuple = ()
-    if len(sentence) == 0:
-        s_tuple = 0, None
+    if sentence != '':
+        first_char = sentence[0]
     else:
-        s_tuple = len(sentence), sentence[0]
-        return s_tuple
+        first_char = None
+    return (len(sentence), first_char)
