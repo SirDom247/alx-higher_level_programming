35729e494dafdbd1bf26a2fa728e00ad59f0d0a1
SirDom247 <dominicudousoro@gmail.com>
2023-04-18 15:57:20 -0700

    Python Almost a Circle

--- 0x0C-python-almost_a_circle/tests/test_models/test_square.py
+++ 0x0C-python-almost_a_circle/tests/test_models/test_square.py
@@ -1,134 +1,77 @@
+
 #!/usr/bin/python3
-"""Defines a class TestSquareMethods"""
+"""
+Test for the Square class
+"""
 
-
-from unittest.mock import patch
 import unittest
+import pep8
+import inspect
+import io
 import json
-from io import StringIO
-from models.base import Base
-from models.rectangle import Rectangle
+import os
+from contextlib import redirect_stdout
 from models.square import Square
+from models.base import Base
 
 
-class TestSquareMethods(unittest.TestCase):
-    """ Defines tests for Square class """
+# class Square(Rectangle):
+#     """Class Square"""
 
-    def setUp(self):
-        """ Method invoked for each test """
-        Base._Base__nb_objects = 0
+#     def __init__(self, size, x=0, y=0, id=None):
+#         super().__init__(size, size, x, y, id)
 
-    def tearDown(self):
-        """ Cleans up after each test """
-        pass
+#     def __str__(self):
+#         """ Special Method STR """
+#         st = "[Square] ({:d}) {:d}/{:d} - {:d}"
+#         return st.format(self.id, self.x, self.y, self.width)
 
-    def test_new_square(self):
-        """ Test new square """
-        s1 = Square(3)
-        s2 = Square(1, 2, 3, 4)
-        self.assertEqual(s1.size, 3)
-        self.assertEqual(s1.width, 3)
-        self.assertEqual(s1.x, 0)
-        self.assertEqual(s1.y, 0)
-        self.assertEqual(s1.id, 1)
-        self.assertEqual(s2.size, 1)
-        self.assertEqual(s2.width, 1)
-        self.assertEqual(s2.x, 2)
-        self.assertEqual(s2.y, 3)
-        self.assertEqual(s2.id, 4)
+    def test_create(self):
+        """test normal use of create"""
+        s1 = {"id": 2, "size": 3, "x": 4, "y": 0}
+        s2 = {"id": 9, "size": 6, "x": 7, "y": 8}
+        s1c = Square.create(**s1)
+        s2c = Square.create(**s2)
+        self.assertEqual("[Square] (2) 4/0 - 3", str(s1c))
+        self.assertEqual("[Square] (9) 7/8 - 6", str(s2c))
+        self.assertIsNot(s1, s1c)
+        self.assertIsNot(s2, s2c)
+        self.assertNotEqual(s1, s1c)
+        self.assertNotEqual(s2, s2c)
 
-    def test_attributes_1(self):
-        """ Test for width and x and y types"""
-        with self.assertRaisesRegex(TypeError, "width must be an integer"):
-            Square("1")
-        with self.assertRaisesRegex(TypeError, "x must be an integer"):
-            Square(1, "2")
-        with self.assertRaisesRegex(TypeError, "y must be an integer"):
-            Square(1, 2, "3")
+    def test_load_from_file_no_file(self):
+        """Checks use of load_from_file with no file"""
+        try:
+            os.remove("Square.json")
+        except:
+            pass
+        self.assertEqual(Square.load_from_file(), [])
 
-    def test_attributes_2(self):
-        """ Test for width and height ranges"""
-        with self.assertRaisesRegex(ValueError, "width must be > 0"):
-            Square(-1)
-            Square(0)
-        with self.assertRaisesRegex(ValueError, "x must be >= 0"):
-            Square(1, -2)
-        with self.assertRaisesRegex(ValueError, "y must be >= 0"):
-            Square(1, 2, -3)
-
-    def test_constructor_no_args(self):
-        """ Tests constructor with no args """
-        with self.assertRaises(TypeError) as e:
-            r = Square()
-        s = "__init__() missing 1 required positional argument: 'size'"
-        self.assertEqual(str(e.exception), s)
-
-    def test_C_constructor_many_args(self):
-        """ Tests constructor with many arguments """
-        with self.assertRaises(TypeError) as e:
-            r = Square(1, 2, 3, 4, 5)
-        s = "__init__() takes from 2 to 5 positional arguments but 6 \
-were given"
-        self.assertEqual(str(e.exception), s)
-
-    def test_is_Rectangle_instance(self):
-        """ Test Square is a Rectangle instance """
-        s1 = Square(1)
-        self.assertEqual(True, isinstance(s1, Rectangle))
-
-    def test_area(self):
-        """ Test area method """
-        s1 = Square(4)
-        self.assertEqual(s1.area(), 16)
-
-    def test_area_2(self):
-        """ Test area method after modifying size """
-        r1 = Square(4)
-        self.assertEqual(r1.area(), 16)
-        r1.size = 9
-        self.assertEqual(r1.area(), 81)
-
-    def test_area_no_args(self):
-        """ Test area method with no arguments"""
-        r = Square(5)
-        with self.assertRaises(TypeError) as e:
-            Square.area()
-        s = "area() missing 1 required positional argument: 'self'"
-        self.assertEqual(str(e.exception), s)
+    def test_load_from_file_empty_file(self):
+        """Checks use of load_from_file with empty file"""
+        try:
+            os.remove("Square.json")
+        except:
+            pass
+        open("Square.json", 'a').close()
+        self.assertEqual(Square.load_from_file(), [])
 
     def test_load_from_file(self):
-        """ Test load JSON file """
-        load_file = Square.load_from_file()
-        self.assertEqual(load_file, load_file)
-
-    def test_basic_display(self):
-        """ Test display without x and y """
-        s1 = Square(6)
-        result = "######\n######\n######\n######\n######\n######\n"
-        with patch('sys.stdout', new=StringIO()) as str_out:
-            s1.display()
-            self.assertEqual(str_out.getvalue(), result)
-
-    def test_display_no_args(self):
-        """ Test display method with no arguments """
-        r = Square(9)
-        with self.assertRaises(TypeError) as e:
-            Square.display()
-        s = "display() missing 1 required positional argument: 'self'"
-        self.assertEqual(str(e.exception), s)
-
-    def test_str(self):
-        """ Test __str__ return value """
-        s1 = Square(3, 1, 3)
-        result = "[Square] (1) 1/3 - 3\n"
-        with patch('sys.stdout', new=StringIO()) as str_out:
-            print(s1)
-            self.assertEqual(str_out.getvalue(), result)
-
-    def test_str_no_args(self):
-        """ Tests __str__ method with no arguments """
-        r = Square(5, 2)
-        with self.assertRaises(TypeError) as e:
-            Square.__str__()
-        s = "__str__() missing 1 required positional argument: 'self'"
-        self.assertEqual(str(e.exception), s)
+        """test normal use of load_from_file"""
+        s1 = Square(2, 3, 4, 5)
+        s2 = Square(7, 8, 9, 10)
+        li = [s1, s2]
+        Square.save_to_file(li)
+        lo = Square.load_from_file()
+        self.assertTrue(type(lo) is list)
+        self.assertEqual(len(lo), 2)
+        s1c = lo[0]
+        s2c = lo[1]
+        self.assertTrue(type(s1c) is Square)
+        self.assertTrue(type(s2c) is Square)
+        self.assertEqual(str(s1), str(s1c))
+        self.assertEqual(str(s2), str(s2c))
+        self.assertIsNot(s1, s1c)
+        self.assertIsNot(s2, s2c)
+        self.assertNotEqual(s1, s1c)
+        self.assertNotEqual(s2, s2c)
