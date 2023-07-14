48b8e115bd8dd32d532359c55be2dda54fd04af4
Dominic <dominicudousoro@gmail.com>
2023-07-14 12:01:46 -0700

    Arguments

--- 0x12-javascript-warm_up/2-arguments.js
+++ 0x12-javascript-warm_up/2-arguments.js
@@ -1,13 +1,11 @@
 #!/usr/bin/node
-
 const argsLength = process.argv.length;
-
 if (argsLength === 2) {
 	console.log("No argument");
 } 
-else if (argsLength === 3) {
+  else if (argsLength === 3) {
 	console.log("Argument found");
 } 
-else {
+  else {
 	console.log("Arguments found");
 }
