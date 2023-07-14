069430be31c8cec5649960fdad9cd143c25a8aad
Dominic <dominicudousoro@gmail.com>
2023-07-14 16:28:31 -0700

    Argumentum

--- 0x12-javascript-warm_up/2-arguments.js
+++ 0x12-javascript-warm_up/2-arguments.js
@@ -1,12 +1,11 @@
 #!/usr/bin/node
 // Prints a message depending of the number of arguments passed
+
 const argsLength = process.argv.length - 2;
-
 if (argsLength === 0) {
-  console.log("No argument");
+  console.log('No argument');
 } else if (argsLength === 1) {
-  console.log("Argument found");
+  console.log('Argument found');
 } else {
-  console.log("Arguments found");
+  console.log('Arguments found');
 }
-
