fe60f814bc11a86cc8c8cda10c9075795a783bfe
Dominic <dominicudousoro@gmail.com>
2023-07-15 00:54:19 -0700

    Create a sentence

--- 0x12-javascript-warm_up/4-concat.js
+++ 0x12-javascript-warm_up/4-concat.js
@@ -3,8 +3,8 @@
 
 const myArgs = process.argv.slice(2);
 if (!myArgs[0]) {
-    console.log('undefined is undefined');
+	console.log('undefined is undefined');
 }
 else {
-    console.log(myArgs[0], 'is', myArgs[1]);
+	console.log(myArgs[0], 'is', myArgs[1]);
 }
