1dc54b8cb0ff28e6d8ffa030db119c3b45f7e12c
Dominic <dominicudousoro@gmail.com>
2023-07-14 18:08:21 -0700

    creat a sentence

---
+++ 0x12-javascript-warm_up/4-concat.js
@@ -0,0 +1,7 @@
+#!/usr/bin/node
+// A script that concatenates the values of two arguments passed to it.
+
+const argument1 = process.argv[2];
+const argument2 = process.argv[3];
+console.log(`${argument1} is ${argument2}`);
+
