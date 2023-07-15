df594b22f98085f47436ee338fda37902e6e6dc1
Dominic <dominicudousoro@gmail.com>
2023-07-15 10:58:15 -0700

    Second bigest search

--- 0x12-javascript-warm_up/11-second_biggest.js
+++ 0x12-javascript-warm_up/11-second_biggest.js
@@ -1,5 +1,7 @@
 #!/usr/bin/node
-//const process = require('process');
+// A script that searches for the second biggest integer in the list of arguments.
+
+const process = require('process');
 const argv = process.argv;
 const argc = process.argv.length;
 if (argc < 4) {
