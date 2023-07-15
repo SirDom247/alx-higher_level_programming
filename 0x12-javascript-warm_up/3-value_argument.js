e2b08f82b204b98080959c90baa75c9064483f5f
Dominic <dominicudousoro@gmail.com>
2023-07-14 17:57:43 -0700

    First argument

--- 0x12-javascript-warm_up/3-value_argument.js
+++ 0x12-javascript-warm_up/3-value_argument.js
@@ -2,10 +2,8 @@
 // A scrit that prints the value of the first argument passed
 
 const argument = process.argv[2];
-
 if (argument) {
   console.log(argument);
 } else {
   console.log('No argument');
 }
-
