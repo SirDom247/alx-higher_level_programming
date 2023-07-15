778d9ed0b7d6e1d02e366e4e265e022351201ff9
Dominic <dominicudousoro@gmail.com>
2023-07-14 18:30:10 -0700

    Concatenation in JS

--- 0x12-javascript-warm_up/4-concat.js
+++ 0x12-javascript-warm_up/4-concat.js
@@ -2,7 +2,7 @@
 // A script that concatenates the values of two arguments passed to it.
 
 const myArgs = process.argv.slice(2);
-i (!myArgs[0]) {
+if (!myArgs[0]) {
     console.log("undefined is undefined");
 }
 else {
