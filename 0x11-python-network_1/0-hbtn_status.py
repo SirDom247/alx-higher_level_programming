03e6b5f3fd0b4490824e4bdff76ca9e37959515d
SirDom247 <dominicudousoro@gmail.com>
2023-08-15 00:33:47 +0100

    fetches https://alx-intranet.hbtn.io/status

--- 0x11-python-network_1/0-hbtn_status.py
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -1,9 +1,10 @@
 #!/usr/bin/python3
 """This script fetches https://alx-intranet.hbtn.io/status using urllib package only"""
-from urllib import request
 
-if __name__ == "__main__":
-    with request.urlopen('https://alx-intranet.hbtn.io/status') as res:
+if __name__ == '__main__':
+    import urllib.request
+
+    with urllib.request.urlopen('https://alx-intranet.hbtn.io/status') as res:
         content = res.read()
         print("Body response:")
         print("\t- type: {}".format(type(content)))
