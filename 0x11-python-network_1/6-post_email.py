2f27d360feaf2b21df70f71301a69f9bdd2e87ed
SirDom247 <dominicudousoro@gmail.com>
2023-08-15 11:17:40 +0000

    post email

--- 0x11-python-network_1/6-post_email.py
+++ 0x11-python-network_1/6-post_email.py
@@ -4,9 +4,13 @@ sends a POST request to the passed URL with the email as a
 parameter, and finally displays the body of the response.
 It uses the packages 'requests' and 'sys' only.
 """
-
 import requests
 from sys import argv
 
+
 if __name__ == "__main__":
-    print(requests.post(argv[1], resp={'email': argv[2]}).text)
+    url = argv[1]
+    value = {"email": argv[2]}
+    response = requests.post(url, data=value)
+
+    print(response.text)
