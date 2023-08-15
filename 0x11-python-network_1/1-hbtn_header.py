bcf368b594b330e491318c02783fd760652f310f
Dominic <dominicudousoro@gmail.com>
2023-08-15 00:38:57 +0000

    Response header value #0

--- 0x11-python-network_1/1-hbtn_header.py
+++ 0x11-python-network_1/1-hbtn_header.py
@@ -12,4 +12,4 @@ if __name__ == "__main__":
 
     request = urllib.request.Request(url)
     with urllib.request.urlopen(request) as res:
-        print(dict(response.headers).get("X-Request-Id"))
+        print(dict(res.headers).get("X-Request-Id"))
