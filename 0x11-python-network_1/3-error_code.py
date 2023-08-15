0a6f0cc724ee49558a3a4b2deafc7cf9860723e8
Dominic <dominicudousoro@gmail.com>
2023-08-14 20:02:56 -0700

    Error code

--- 0x11-python-network_1/3-error_code.py
+++ 0x11-python-network_1/3-error_code.py
@@ -1,16 +1,18 @@
 #!/usr/bin/python3
-"""This is Python script that takes in a URLand an email, sends a POST
-request to the passed URL with the email as a parameter, and
-displays the body of the response (decoded in utf-8)"""
+"""This is a Python script that takes in a URL, sends a request to the URL, and
+displays the body of the response (decoded in utf-8). It handles HTTP errors
+and prints the error code if an HTTP error occurs."""
 
 from urllib import request
-from urllib import error
-from sys import argv
+from urllib.error import HTTPError
+import sys
 
 if __name__ == "__main__":
-        try:
-            with request.urlopen(argv[1]) as page:
-                print(page.read().decode('utf-8'))
-        except error.HTTPError as e:
-            print("Error code: {}".format(e.code))
+    url = sys.argv[1]
+
+    try:
+        with request.urlopen(url) as response:
+            print(response.read().decode('utf-8'))
+    except HTTPError as e:
+        print("Error code:", e.code)
 
