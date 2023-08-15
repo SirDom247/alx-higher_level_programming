aed2f7a8a4e20f18646a96973f32165b890c076d
SirDom247 <dominicudousoro@gmail.com>
2023-08-15 08:47:44 +0000

    What's my status? #1

--- 0x11-python-network_1/4-hbtn_status.py
+++ 0x11-python-network_1/4-hbtn_status.py
@@ -4,13 +4,11 @@
 - use the package 'requests' only,
 - displaying the body of the response in 'tabulation before -' format.
 """
-import urllib.request
-import urllib.error
-import sys.argv
+import requests
 
-if __name__ == "__main__":
-    try:
-        with request.urlopen(argv[1]) as page:
-            print(page.read().decode('utf-8'))
-    except error.HTTPError as e:
-        print("Error code: {}".format(e.code))
+if __name__ == '__main__':
+
+    html = requests.get('https://alx-intranet.hbtn.io/status')
+    print("Body response:")
+    print("\t- type: {}".format(html.text.__class__))
+    print("\t- content: {}".format(html.text))
