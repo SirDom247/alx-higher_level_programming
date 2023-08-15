8efe4e8cef7507f19f417ea5efea524f1f0445d6
SirDom247 <dominicudousoro@gmail.com>
2023-08-15 08:37:55 +0000

    What's my status? #1

--- 0x11-python-network_1/4-hbtn_status.py
+++ 0x11-python-network_1/4-hbtn_status.py
@@ -1,16 +1,15 @@
 #!/usr/bin/python3
-"""This is a Python script that 
+"""This is a Python script that
 - fetches https://alx-intranet.hbtn.io/status
-- use the package 'requests' only, 
+- use the package 'requests' only,
 - displaying the body of the response in 'tabulation before -' format.
 """
 from urllib import request, error
 from sys import argv
 
 if __name__ == "__main__":
-        try:
-            with request.urlopen(argv[1]) as page:
-                print(page.read().decode('utf-8'))
-        except error.HTTPError as e:
-            print("Error code: {}".format(e.code))
-
+    try:
+        with request.urlopen(argv[1]) as page:
+            print(page.read().decode('utf-8'))
+    except error.HTTPError as e:
+        print("Error code: {}".format(e.code))
