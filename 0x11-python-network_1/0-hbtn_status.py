bcab446d67ba0821b51818a006ac964a294c0cd7
Dominic <dominicudousoro@gmail.com>
2023-08-15 00:07:08 +0000

    fetches https://alx-intranet.hbtn.io/status

--- 0x11-python-network_1/0-hbtn_status.py
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -1,12 +1,15 @@
 #!/usr/bin/python3
-"""This script fetches https://alx-intranet.hbtn.io/status using urllib package only"""
+"""A script that
+- fetches https://alx-intranet.hbtn.io/status.
+- uses urlib package
+"""
 import urllib.request
 
-url = "https://alx-intranet.hbtn.io/status"
+if __name__ == '__main__':
 
-with urllib.request.urlopen(url) as response:
-    content = response.read().decode('utf-8')
-
-    print("Body response:")
-    print("\t- type:", type(content))
-    print("\t- content:", content)
+    with urllib.request.urlopen('https://alx-intranet.hbtn.io/status') as res:
+        content = res.read()
+        print("Body response:")
+        print("\t- type: {}".format(type(content)))
+        print("\t- content: {}".format(content))
+        print("\t- utf8 content: {}".format(content.decode('utf-8')))
