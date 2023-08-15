ceb8fd245674d56ce9d8c523e7494e3125c2049d
Dominic <dominicudousoro@gmail.com>
2023-08-15 08:10:12 +0000

    Error code

--- 0x11-python-network_1/3-error_code.py
+++ 0x11-python-network_1/3-error_code.py
@@ -1,5 +1,7 @@
 #!/usr/bin/python3
-"""This is a script that takes in a URL, sends a request to the URLdisplays the body of the response (decoded in utf-8).
+"""This is a script that takes in a URL,
+    sends a request to the URL
+    and displays the body of the response (decoded in utf-8).
 """
 import sys
 from urllib import request, error
